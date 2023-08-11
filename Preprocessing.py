a45c79fd034e676e1f657d0bd8aeae7bf3974d83
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2023-08-12 01:13:41 +0500

    partially succesfull attempt to remove blue marks from the photo

--- Preprocessing.py
+++ Preprocessing.py
@@ -13,9 +13,10 @@ Following are the DHR tasks followed in this example code:
 import cv2
 import glob
 from PIL import Image, ImageFilter
+import numpy as np
 
-ddi_clean = "D:/DigitalHairRemoval/DDI_Clean/*.png"
-ddi_clean_output = "D:/DigitalHairRemoval/DDI_Clean_Output/"
+ddi_clean = "D:/SkinLesionClassification/DDI_Clean/*.png"
+ddi_clean_output = "D:/SkinLesionClassification/DDI_Clean_Output/"
 imgs = []
 
 for filePath in glob.glob(ddi_clean):
@@ -50,9 +51,29 @@ for filePath in glob.glob(ddi_clean):
 
     # # Now applying Median Filter to Smoothen up the image
     # # creating an image object
-    imgForMedianFilter = Image.open(filePath)
+    imgForMedianFilter = Image.open(ddi_clean_output+"Inpainted/"+filename)
     medianFilteredImage = imgForMedianFilter.filter(ImageFilter.MedianFilter(size=3))
     medianFilteredImage.save(ddi_clean_output+"MedianFilteredImages/"+filename)
-    # imgForMedianFilter.save(ddi_clean_output+"MedianFilteredImages/"+filename)
-    # cv2.imwrite(ddi_clean_output+"MedianFilteredImages/"+filename, medianFilteredImage,
-    #             [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+
+    # Removing Blue marks from the photo using replacement pixels technique and inpainting
+    # Method 1: Simplistic overpaint blue with white
+    imageForBlueRemoval = cv2.imread(ddi_clean_output+"MedianFilteredImages/"+filename)
+    # Define lower and upper limits of our blue
+    # BlueMin = np.array([90, 200, 200], np.uint8)
+    # BlueMax = np.array([100, 255, 255], np.uint8)
+    #
+    # # Go to HSV colourspace and get mask of blue pixels
+    # HSV = cv2.cvtColor(imageForBlueRemoval, cv2.COLOR_BGR2HSV)
+    # mask = cv2.inRange(HSV, BlueMin, BlueMax)
+    #
+    # SE = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    # mask = cv2.dilate(mask, kernel, iterations=1)
+    #
+    # # Make all pixels in mask white
+    # imageForBlueRemoval[mask > 0] = [255, 255, 255]
+
+    imageForBlueRemoval[np.all(imageForBlueRemoval == (255, 0, 0), axis=-1)] = np.uint8([255, 255, 255])
+    mask = np.all(imageForBlueRemoval == (255, 0, 0), axis=-1)
+    blueDotsRemovedImage = cv2.inpaint(imageForBlueRemoval, np.uint8(mask) * 255, 3, cv2.INPAINT_TELEA)
+    cv2.imwrite(ddi_clean_output+"BlueMarksRemoved/"+filename, blueDotsRemovedImage,
+                [int(cv2.IMWRITE_JPEG_QUALITY), 90])
