09350ee503766be1837fdcd35124450f787fb08f
Mahnoor Khan <mahnoorkhan_hallian@yahoo.com>
2023-08-19 01:27:44 +0500

    I declared a function to augment images and gave it folder paths of input and output images as arguments.

--- Augmentation.py
+++ Augmentation.py
@@ -26,10 +26,9 @@ outputPathBenign = "D:/MS Thesis/SkinLesionDetectionCode/Benign/"
 outputPathMalignant = "D:/MS Thesis/SkinLesionDetectionCode/Malignant/"
 
 def augment(folderPath, outputPath, iteration):
-    total_malignant_black = 5
     data_gen = ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.1,horizontal_flip=True,vertical_flip=True)
     # now taking image one by one and converting it to tensor
-    for filePath in glob.glob(pathMalignantBlack):
+    for filePath in glob.glob(folderPath):
         srcImg = load_img(filePath, grayscale=False)
         imgArray = img_to_array(srcImg)
         filename = filePath.partition("\\")[2]
@@ -37,8 +36,8 @@ def augment(folderPath, outputPath, iteration):
 
         for i, _ in enumerate(data_gen.flow(x=tensorImage,
                                             batch_size=1,
-                                            save_to_dir=pathMalignantAugmented+"Black/Augmented",
-                                            save_prefix="generated",
+                                            save_to_dir=outputPath,
+                                            save_prefix=filename+"_",
                                             save_format=".png")):
-            if i >= total_malignant_black:
+            if i >= iteration:
                 break
