9342c02097032d4b69af02b2ec7817e598ac985f
Mahnoor Khan <mahnoorkhan_hallian@yahoo.com>
2023-08-20 16:32:25 +0500

    Saved image files in X_test, X-train and X-validdation to testing, training and validation folders.

--- Stratification.py
+++ Stratification.py
@@ -7,9 +7,23 @@
 
 from sklearn.model_selection import StratifiedShuffleSplit
 import pandas as pd
+from PIL import Image, ImageFilter
+
+
+def saveimages(test_images, original_image_path, test_dir):
+    names = test_images.DDI_file
+    for file_name in names:
+        full_image_path = original_image_path + "/" + file_name
+        image_file = Image.open(full_image_path);
+        image_file.save(test_dir + "/" + file_name)
 
 
 def stratify(filePath):
+    image_path = "E:/DDI/DDI_Clean_Output/DDI_original/"
+    testing = "D:/MS Thesis/SkinLesionDetectionCode/Testing"
+    training = "D:/MS Thesis/SkinLesionDetectionCode/Training"
+    validation = "D:/MS Thesis/SkinLesionDetectionCode/Validation"
+
     data = pd.read_csv(filePath)
     X = data.drop("malignant", axis=1)
     y = data["malignant"]
@@ -22,15 +36,16 @@ def stratify(filePath):
         y_train = y.loc[train_index]
         y_test = y.loc[test_index]
 
-
-
-    # Split the data into training and test sets
+    saveimages(X_test, image_path, testing)
+    # Split the data into training and validation
     for train_index, test_index in split.split(X_train, y_train):
         X_train = X.loc[train_index]
         X_validation = X.loc[test_index]
         y_train = y.loc[train_index]
         y_validation = y.loc[test_index]
 
+    saveimages(X_validation, image_path, validation)
+    saveimages(X_train, image_path, training)
 
 def main():
     file_path = 'E:/DDI/ddi_metadata.csv'
