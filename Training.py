87c4e89a86a7b2088fe060e84b300bee98f6fcd6
Rizwan Ullah <rizwan.akhwandzada@gmail.com>
2023-08-24 18:36:02 +0500

    loading train test and validation data, showiing info, caching and shuffling

--- Training.py
+++ Training.py
@@ -19,12 +19,19 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.metrics import roc_curve, roc_auc_score
 
 
-def initialize_data(data_path, IMAGE_SIZE, BATCH_SIZE):
+def initialize_training_dataset(data_path, IMAGE_SIZE, BATCH_SIZE):
     dataset = tf.keras.preprocessing.image_dataset_from_directory(data_path, seed=123, shuffle=True,
                                                                   image_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                                   batch_size=BATCH_SIZE
                                                                   )
-    print(dataset.class_names)
+    return dataset
+
+
+def initialize_dataset(data_path, IMAGE_SIZE, BATCH_SIZE):
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(data_path, labels=None, seed=123, shuffle=True,
+                                                                  image_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                                                  batch_size=BATCH_SIZE
+                                                                  )
     return dataset
 
 
@@ -43,6 +50,24 @@ def visiualize_images(dataset, class_names):
             plt.axis("off")
 
 
+def essential_info(trainingDS, testingDS, validationDS):
+    print(trainingDS.class_names)
+    print(testingDS.class_names)
+    print(validationDS.class_names)
+
+    print(len(trainingDS))
+    print(len(testingDS))
+    print(len(validationDS))
+
+
+def cache_shuffle_prefetch(trainDS, testDS, validationDS):
+    train_ds = trainDS.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    test_ds = testDS.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    val_ds = validationDS.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return train_ds, test_ds, val_ds,
+
+
 def main():
     BATCH_SIZE = 32
     IMAGE_SIZE = 224
@@ -50,9 +75,18 @@ def main():
     EPOCHS = 100
 
     dataset_path = "D:/SkinLesionClassification/TrainingDS"
-    dataset = initialize_data(dataset_path, IMAGE_SIZE, BATCH_SIZE)
-    print_image_label_batch(dataset)
-    visiualize_images(dataset, dataset.class_names)
+    testing_dataset_path = "D:/SkinLesionClassification/Testing/"
+    validation_dataset_path = "D:/SkinLesionClassification/Validation/"
+
+    training_dataset = initialize_training_dataset(dataset_path, IMAGE_SIZE, BATCH_SIZE)
+    testing_dataset = initialize_dataset(testing_dataset_path, IMAGE_SIZE, BATCH_SIZE)
+    validation_dataset = initialize_dataset(validation_dataset_path, IMAGE_SIZE, BATCH_SIZE)
+
+    print_image_label_batch(training_dataset)
+    visiualize_images(training_dataset, training_dataset.class_names)
+    essential_info(training_dataset, testing_dataset, validation_dataset)
+
+    trainDS, testDS, validationDS = cache_shuffle_prefetch(training_dataset, testing_dataset, validation_dataset)
 
 
 if __name__ == "__main__":
